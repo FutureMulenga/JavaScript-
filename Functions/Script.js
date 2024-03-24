10f028f791e44e01fa88e316bf0ff2b3ecc1b26c
Future Mulenga <mulengafuture14@gmail.com>
2024-03-24 00:13:35 -1000

    working with objects

--- Functions/Script.js
+++ Functions/Script.js
@@ -1,4 +1,13 @@
-function name(){
 
-    console.log("my name is Future Mulenga");
+/*NAME: FUTURE MULENGA
+  PURPOSE: Learnig javascript
+  GITHUB: https://github.com/FutureMulenga/JavaScript-.git
+*/
+
+
+function Name(first_name, last_name){
+
+    console.log("Hello" + " " + first_name + " " + last_name);
 }
+
+Name("Future", "Mulenga");// passing the arguments into the function
---
+++ Objects/object.js
@@ -0,0 +1,14 @@
+/*NAME: FUTURE MULENGA
+  PURPOSE: Working with objects 
+  GITHUB: https://github.com/FutureMulenga/JavaScript-.git
+*/
+
+
+const Person ={
+     
+    first_name: "Future",
+    last_name: "Mulenga",
+    age_: 22
+}
+
+console.log(Person);
